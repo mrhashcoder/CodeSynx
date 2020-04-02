e697824d083369d935b3048b77a19e558b1a2945
mrhashcoder <abhie8101@gmail.com>
2020-04-03 00:14:11 +0530

    socket setup done

--- index.js
+++ index.js
@@ -15,20 +15,28 @@ const codeSynxRouter = require("./routes/codesynx");
 app.use(codeSynxRouter);
 
 
+//serving static files
+app.use(express.static('scripts'));
 
 //setting database
 
 //starting server
 var server = app.listen('4000' , ()=>{
     console.log('server started at 4000');
-})
-
+});
 
 //settings sockets
 
 var io = socket(server);
+
 io.on('connection' , function(socket){
-    console.log("made a connection :" + socket.id);
+    //console.log("made a connection :" + socket.id);
+    socket.on('code' , function(data){
+        var roomId = data.synxid;
+        socket.join(roomId);
+        io.to(roomId).emit('code' , data);
+        //console.log(data);
+    });
 });
 
 
--- scripts/codeSynxSocket.js
+++ scripts/codeSynxSocket.js
@@ -1,1 +1,2 @@
-//console.log(window.location.href)
+console.log('hello world');
+console.log("hello fucking shit");
